d34a3e49488169c73d787b93125841dc87c38879
rolandhe <rolandhe@qq.com>
2017-05-28 15:05:21 +0800

    fix bit bug

--- hiriver-modules/mysql-proto/src/main/java/com/hiriver/unbiz/mysql/lib/protocol/binary/BitColumnTypeValueParser.java
+++ hiriver-modules/mysql-proto/src/main/java/com/hiriver/unbiz/mysql/lib/protocol/binary/BitColumnTypeValueParser.java
@@ -3,6 +3,7 @@ package com.hiriver.unbiz.mysql.lib.protocol.binary;
 import com.hiriver.unbiz.mysql.lib.output.ColumnDefinition;
 import com.hiriver.unbiz.mysql.lib.protocol.Position;
 import com.hiriver.unbiz.mysql.lib.protocol.datautils.MysqlNumberUtils;
+import com.hiriver.unbiz.mysql.lib.protocol.datautils.MysqlStringUtils;
 
 /**
  * bit类型的数据解析器
@@ -14,12 +15,20 @@ public class BitColumnTypeValueParser implements ColumnTypeValueParser {
 
     @Override
     public Object parse(byte[] buf, Position pos, ColumnDefinition columnDef, int meta) {
-        int nbits = ((meta >> 8) * 8) + (meta & 0xff);
+        int nbits = ((meta >>> 8) * 8) + (meta & 0xff);
         int len = (nbits + 7) / 8;
-        if (nbits > 1) {
-            return MysqlNumberUtils.readNInt(buf, pos, len);
+        byte[] bitBuff = MysqlStringUtils.readFixString(buf, pos, len);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < bitBuff.length;i++){
+            int v = bitBuff[i] & 0xff;
+            if(i == 0) {
+                sb.append(Integer.toBinaryString(v));
+                continue;
+            }
+            String pad = "00000000" + Integer.toBinaryString(v);
+            sb.append(pad.substring(pad.length() - 8));
         }
-        return MysqlNumberUtils.read1Int(buf, pos);
+        return "0b" + sb.toString();
     }
 
 }
